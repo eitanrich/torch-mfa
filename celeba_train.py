cd1257bbd89c9f653265d27944a3f13e1ffccd8e
eitanrich <eitan.richardson@gmail.com>
2019-11-04 12:41:11 +0200

    Cleaning up celeba training code

--- celeba_train.py
+++ celeba_train.py
@@ -4,44 +4,47 @@ from torchvision.datasets import CelebA
 import torchvision.transforms as transforms
 from mfa import MFA
 from utils import CropTransform, ReshapeTransform, samples_to_mosaic, visualize_model
-import matplotlib
-if os.name == 'posix' and "DISPLAY" not in os.environ:
-    matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from imageio import imwrite
 
+image_size = 64                 # The image width and height (assumed equal here)
+n_components = 300              # Number of components in the mixture model
+n_factors = 10                  # Number of factors - the latent dimension (same for all components)
+batch_size = 1000               # The EM batch size
+num_iterations = 2             # Number of EM iterations (=epochs)
+responsibility_sampling = 0.2   # For faster responsibilities calculation, randomly sample the coordinates
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-w = 64
 model_dir = './models/celeba'
-trans = transforms.Compose([CropTransform((25, 50, 25+128, 50+128)), transforms.Resize(w), transforms.ToTensor(),
-                            ReshapeTransform([-1])])
-train_set = CelebA(root='/mnt/local/eitanrich/PhD/Datasets/CelebA', transform=trans)
+os.makedirs(model_dir, exist_ok=True)
 
-for n_components in [100]:
-    for n_factors in [10]:
-        for batch_size in [1000]:   #[1000, 10000]:
-            for try_num in range(1):
-                model = MFA(n_components=n_components, n_features=w*w*3, n_factors=n_factors).to(device=device)
+print('Preparing the dataset...')
+trans = transforms.Compose([CropTransform((25, 50, 25+128, 50+128)), transforms.Resize(image_size),
+                            transforms.ToTensor(),  ReshapeTransform([-1])])
+train_set = CelebA(root='data/CelebA', split='train', transform=trans, download=True)
+test_set = CelebA(root='data/CelebA', split='test', transform=trans, download=True)
 
-                print('EM fitting: {} components / {} factors / batch size {} / try {}...'.format(
-                    n_components, n_factors, batch_size, try_num))
+print('Defining the MFA model...')
+model = MFA(n_components=n_components, n_features=image_size*image_size*3, n_factors=n_factors).to(device=device)
 
-                ll_log = model.batch_fit(train_set, batch_size=batch_size, max_iterations=10, responsibility_sampling=0.2)
+print('EM fitting: {} components / {} factors / batch size {} ...'.format(n_components, n_factors, batch_size))
+ll_log = model.batch_fit(train_set, test_set, batch_size=batch_size, max_iterations=num_iterations,
+                         responsibility_sampling=responsibility_sampling)
 
-                print('Saving model...')
-                torch.save(model.state_dict(), os.path.join(model_dir, 'model_c_{}_l_{}.pth'.format(n_components, n_factors)))
+print('Saving model...')
+torch.save(model.state_dict(), os.path.join(model_dir, 'model_c_{}_l_{}.pth'.format(n_components, n_factors)))
 
-                plt.plot(ll_log, label='c{}_l{}_b{}'.format(n_components, n_factors, batch_size))
-                plt.grid(True)
-                plt.pause(0.1)
-                rnd_samples, _ = model.sample(100, with_noise=False)
-                mosaic = samples_to_mosaic(rnd_samples, image_shape=[w, w, 3])
-                imwrite(os.path.join(model_dir, 'samples_c_{}_l_{}_b_{}_try_{}.jpg'.format(
-                    n_components, n_factors, batch_size, try_num)), mosaic)
-                model_image = visualize_model(model, image_shape=[w, w, 3], end_component=10)
-                imwrite(os.path.join(model_dir,
-                            'model_c_{}_l_{}_try_{}.jpg'.format(n_components, n_factors, try_num)), model_image)
+print('Visualizing the trained model...')
+model_image = visualize_model(model, image_shape=[image_size, image_size, 3], end_component=10)
+imwrite(os.path.join(model_dir, 'model_c_{}_l_{}.jpg'.format(n_components, n_factors)), model_image)
+
+print('Generating random samples...')
+rnd_samples, _ = model.sample(100, with_noise=False)
+mosaic = samples_to_mosaic(rnd_samples, image_shape=[image_size, image_size, 3])
+imwrite(os.path.join(model_dir, 'samples_c_{}_l_{}.jpg'.format(n_components, n_factors)), mosaic)
+
+print('Plotting test log-likelihood graph...')
+plt.plot(ll_log, label='c{}_l{}_b{}'.format(n_components, n_factors, batch_size))
+plt.grid(True)
+plt.savefig(os.path.join(model_dir, 'training_graph_c_{}_l_{}.jpg'.format(n_components, n_factors)))
 print('Done')
-plt.savefig('celeba_batches.pdf')
-plt.show()
