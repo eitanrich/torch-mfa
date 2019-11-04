8c1ca9e99e95a6f760f5dc602b6c8c0157a086e3
eitanrich <eitan.richardson@gmail.com>
2019-11-04 18:20:11 +0200

    merging celeba and mnist training

--- train.py
+++ train.py
@@ -1,58 +1,79 @@
-import os
+import sys, os
 import torch
-from torchvision.datasets import CelebA
+from torchvision.datasets import CelebA, MNIST
 import torchvision.transforms as transforms
+import numpy as np
 from mfa import MFA
 from utils import CropTransform, ReshapeTransform, samples_to_mosaic, visualize_model
 from matplotlib import pyplot as plt
 from imageio import imwrite
 
-image_size = 64                 # The image width and height (assumed equal here)
-n_components = 200              # Number of components in the mixture model
-n_factors = 10                  # Number of factors - the latent dimension (same for all components)
-batch_size = 1000               # The EM batch size
-num_iterations = 20             # Number of EM iterations (=epochs)
-responsibility_sampling = 0.2   # For faster responsibilities calculation, randomly sample the coordinates (or False)
-mfa_sgd_epochs = 0              # Perform additional training with diagonal (per-pixel) covariance, using SGD
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-model_dir = './models/celeba'
-os.makedirs(model_dir, exist_ok=True)
+def main(argv):
+    dataset = argv[1] if len(argv) == 2 else 'celeba'
+    print('Preparing dataset and parameters for', dataset, '...')
 
-print('Preparing the dataset...')
-trans = transforms.Compose([CropTransform((25, 50, 25+128, 50+128)), transforms.Resize(image_size),
-                            transforms.ToTensor(),  ReshapeTransform([-1])])
-train_set = CelebA(root='data/CelebA', split='train', transform=trans, download=True)
-test_set = CelebA(root='data/CelebA', split='test', transform=trans, download=True)
+    if dataset == 'celeba':
+        image_shape = [64, 64, 3]       # The input image shape
+        n_components = 200              # Number of components in the mixture model
+        n_factors = 10                  # Number of factors - the latent dimension (same for all components)
+        batch_size = 1000               # The EM batch size
+        num_iterations = 10             # Number of EM iterations (=epochs)
+        responsibility_sampling = 0.2   # For faster responsibilities calculation, randomly sample the coordinates (or False)
+        mfa_sgd_epochs = 0              # Perform additional training with diagonal (per-pixel) covariance, using SGD
+        trans = transforms.Compose([CropTransform((25, 50, 25+128, 50+128)), transforms.Resize(image_shape[0]),
+                                    transforms.ToTensor(),  ReshapeTransform([-1])])
+        train_set = CelebA(root='./data', split='train', transform=trans, download=True)
+        test_set = CelebA(root='./data', split='test', transform=trans, download=True)
+    elif dataset == 'mnist':
+        image_shape = [28, 28]          # The input image shape
+        n_components = 50               # Number of components in the mixture model
+        n_factors = 6                   # Number of factors - the latent dimension (same for all components)
+        batch_size = 1000               # The EM batch size
+        num_iterations = 30             # Number of EM iterations (=epochs)
+        responsibility_sampling = False # For faster responsibilities calculation, randomly sample the coordinates (or False)
+        mfa_sgd_epochs = 0              # Perform additional training with diagonal (per-pixel) covariance, using SGD
+        trans = transforms.Compose([transforms.ToTensor(),  ReshapeTransform([-1])])
+        train_set = MNIST(root='./data', train=True, transform=trans, download=True)
+        test_set = MNIST(root='./data', train=False, transform=trans, download=True)
+    else:
+        assert False, 'Unknown dataset: ' + dataset
 
-print('Defining the MFA model...')
-model = MFA(n_components=n_components, n_features=image_size*image_size*3, n_factors=n_factors).to(device=device)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model_dir = './models/'+dataset
+    os.makedirs(model_dir, exist_ok=True)
 
-print('EM fitting: {} components / {} factors / batch size {} ...'.format(n_components, n_factors, batch_size))
-ll_log = model.batch_fit(train_set, test_set, batch_size=batch_size, max_iterations=num_iterations,
-                         responsibility_sampling=responsibility_sampling)
+    print('Defining the MFA model...')
+    model = MFA(n_components=n_components, n_features=np.prod(image_shape), n_factors=n_factors).to(device=device)
 
-if mfa_sgd_epochs > 0:
-    print('Continuing training using SGD with diagonal (instead of isotropic) noise covariance...')
-    model.isotropic_noise = False
-    ll_log_sgd = model.sgd_mfa_train(train_set, test_size=256, max_epochs=mfa_sgd_epochs,
-                                     responsibility_sampling=responsibility_sampling)
-    ll_log += ll_log_sgd
+    print('EM fitting: {} components / {} factors / batch size {} ...'.format(n_components, n_factors, batch_size))
+    ll_log = model.batch_fit(train_set, test_set, batch_size=batch_size, max_iterations=num_iterations,
+                             responsibility_sampling=responsibility_sampling)
 
-print('Saving the model...')
-torch.save(model.state_dict(), os.path.join(model_dir, 'model_c_{}_l_{}.pth'.format(n_components, n_factors)))
+    if mfa_sgd_epochs > 0:
+        print('Continuing training using SGD with diagonal (instead of isotropic) noise covariance...')
+        model.isotropic_noise = False
+        ll_log_sgd = model.sgd_mfa_train(train_set, test_size=256, max_epochs=mfa_sgd_epochs,
+                                         responsibility_sampling=responsibility_sampling)
+        ll_log += ll_log_sgd
 
-print('Visualizing the trained model...')
-model_image = visualize_model(model, image_shape=[image_size, image_size, 3], end_component=10)
-imwrite(os.path.join(model_dir, 'model_c_{}_l_{}.jpg'.format(n_components, n_factors)), model_image)
+    print('Saving the model...')
+    torch.save(model.state_dict(), os.path.join(model_dir, 'model_c_{}_l_{}.pth'.format(n_components, n_factors)))
 
-print('Generating random samples...')
-rnd_samples, _ = model.sample(100, with_noise=False)
-mosaic = samples_to_mosaic(rnd_samples, image_shape=[image_size, image_size, 3])
-imwrite(os.path.join(model_dir, 'samples_c_{}_l_{}.jpg'.format(n_components, n_factors)), mosaic)
+    print('Visualizing the trained model...')
+    model_image = visualize_model(model, image_shape=image_shape, end_component=10)
+    imwrite(os.path.join(model_dir, 'model_c_{}_l_{}.jpg'.format(n_components, n_factors)), model_image)
 
-print('Plotting test log-likelihood graph...')
-plt.plot(ll_log, label='c{}_l{}_b{}'.format(n_components, n_factors, batch_size))
-plt.grid(True)
-plt.savefig(os.path.join(model_dir, 'training_graph_c_{}_l_{}.jpg'.format(n_components, n_factors)))
-print('Done')
+    print('Generating random samples...')
+    rnd_samples, _ = model.sample(100, with_noise=False)
+    mosaic = samples_to_mosaic(rnd_samples, image_shape=image_shape)
+    imwrite(os.path.join(model_dir, 'samples_c_{}_l_{}.jpg'.format(n_components, n_factors)), mosaic)
+
+    print('Plotting test log-likelihood graph...')
+    plt.plot(ll_log, label='c{}_l{}_b{}'.format(n_components, n_factors, batch_size))
+    plt.grid(True)
+    plt.savefig(os.path.join(model_dir, 'training_graph_c_{}_l_{}.jpg'.format(n_components, n_factors)))
+    print('Done')
+
+if __name__ == "__main__":
+    main(sys.argv)
